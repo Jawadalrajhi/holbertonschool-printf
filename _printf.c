ac6af7b2d7679e6e60be857f78739322230d1828
root <root@ip-10-42-100-171.ec2.internal>
2025-10-02 13:51:29 +0000

    update

--- _printf.c
+++ _printf.c
@@ -1,51 +1,49 @@
 #include "main.h"
-
-/* minimal printf: handles %c, %s, %% */
+/**
+ * _printf - produces output according to a format
+ * @format: the format string containing characters and specifiers
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-       	va_list ap; int count = 0; 
-	size_t i;
-
-	if (!format) return (-1);
-
-	va_start(ap, format);
-
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			count++;
-			continue;
-		}
-
-		i++;                /* move to specifier */
-		if (!format[i])    /* stray '%' at end */
-		{
-			va_end(ap);
-			return (-1);
-		}
-		if (format[i] == '%') /* print literal '%' */
-		{
-			_putchar('%');
-			count++;
-			continue;
-		}
-
-		{
-			print_func fn = get_printer(format[i]); /* pick printer */
-			if (fn)
-				count += fn(&ap);                  /* call printer */
-			else
-			{
-				/* unknown spec: print it as-is */
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
-		}
-	}
-
-	va_end(ap);
-	return (count);
+va_list ap;
+int count = 0;
+size_t i;
+if (!format)
+return (-1);
+va_start(ap, format);
+for (i = 0; format[i]; i++)
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+count++;
+continue;
+}
+i++;
+if (!format[i])
+{
+va_end(ap);
+return (-1);
+}
+if (format[i] == '%')
+{
+_putchar('%');
+count++;
+continue;
+}
+{
+print_func fn = get_printer(format[i]);
+if (fn)
+count += fn(&ap);
+else
+{
+_putchar('%');
+_putchar(format[i]);
+count += 2;
+}
+}
+va_end(ap);
+return (count);
 }
