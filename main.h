7599be4035192088cccdd44c44edc739c8468f30
root <root@ip-10-42-101-68.ec2.internal>
2025-10-04 03:04:58 +0000

    Update main.h for advanced _printf tasks

--- main.h
+++ main.h
@@ -24,13 +24,25 @@ typedef struct spec
     print_func fn;
 } spec_t;
 
-/* Printers */
+/* Basic Printers (Mandatory Tasks) */
 int print_char(va_list *ap);
 int print_string(va_list *ap);
 int print_percent(va_list *ap);
-int print_int(va_list *ap); /* <-- مهم */
+int print_int(va_list *ap);
+
+/* Advanced Printers (Added Separately) */
+int print_binary(va_list *ap);     /* %b */
+int print_unsigned(va_list *ap);   /* %u */
+int print_octal(va_list *ap);      /* %o */
+int print_hex_lower(va_list *ap);  /* %x */
+int print_hex_upper(va_list *ap);  /* %X */
+int print_pointer(va_list *ap);    /* %p */
+int print_S(va_list *ap);          /* %S */
+int print_reverse(va_list *ap);    /* %r */
+int print_rot13(va_list *ap);      /* %R */
 
 /* Dispatcher */
 print_func get_printer(char spec);
 
 #endif /* MAIN_H */
+
